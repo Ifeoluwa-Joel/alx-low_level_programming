ca08c3fca74cf527061a4d6e314ad1fb9e350b65
Ifeoulwa-Joel <Ifeoluwajoel794@gmail.com>
2022-09-09 00:42:04 -0700

    Sizeof Primitive Data Types in C 2nd Attempt

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,5 @@ printf("Size of a char: %d byte(s)\n", char1);
 printf("Size of an int: %d byte(s)\n", int1);
 printf("Size of a long int: %d byte(s)\n", long_int);
 printf("Size of a long long int: %d byte(s)\n", longLong);
-printf("Size of a float: %d byte(s)\n", char1);
+printf("Size of a float: %d byte(s)\n", float1);
 }
