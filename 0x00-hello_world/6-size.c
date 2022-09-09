b5ab803cd384edd2e03e055716c5236345eeeaca
Ifeoulwa-Joel <Ifeoluwajoel794@gmail.com>
2022-09-09 00:35:48 -0700

    SIZEOF primitive data types in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - main block
+ *Return: 0
+ *Program to print out the size of all the primitive data types
+ */
+int main(void)
+{
+int char1 = sizeof(char);
+int int1 = sizeof(int);
+int long_int = sizeof(long int);
+int longLong = sizeof(long  long int);
+int float1 = sizeof(float);
+
+printf("Size of a char: %d byte(s)\n", char1);
+printf("Size of an int: %d byte(s)\n", int1);
+printf("Size of a long int: %d byte(s)\n", long_int);
+printf("Size of a long long int: %d byte(s)\n", longLong);
+printf("Size of a float: %d byte(s)\n", char1);
+}
