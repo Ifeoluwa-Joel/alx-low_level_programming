74fdd2837b693484908f26dd55510aba286b1645
Ifeoulwa-Joel <Ifeoluwajoel794@gmail.com>
2022-09-09 10:06:22 -0700

    C Program to print alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints English alphabets in lower caps
+* Return: 0 Always (SUCCESS)
+*/
+
+int main(void)
+{
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; 1 < 26; i++)
+{
+putchar(alphabet[i]);
+}
+putchar(\n);
+
+}
