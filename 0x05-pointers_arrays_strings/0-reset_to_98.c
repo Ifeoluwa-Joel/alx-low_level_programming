aeb5b6aad44bd57c44350f22dfe536cdea3c96ba
Ifeoulwa-Joel <Ifeoluwajoel794@gmail.com>
2022-09-19 09:47:28 -0700

    Initial Commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* reset_to_98 - function to reset the value of a variable with pointer
+* @n: input value
+* Return: Void
+*/
+
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Directory under ALX Programmi where I will put all  tasks pertaining to Pointers, Arrays and C Strings
